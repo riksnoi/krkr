619a1d6f0dace0937cb1adf8905b636dcf1b649b
riksnoi <riksnoi@163.com>
2024-05-08 13:00:42 +0800

    run demo

--- krkr-api/src/main/java/com/sky/SkyKrkrApp.java
+++ krkr-api/src/main/java/com/sky/SkyKrkrApp.java
@@ -1,15 +1,15 @@
-package com.starrysky;
+package com.sky;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class StarrySkyKrkrApp {
+public class SkyKrkrApp {
 
     public static void main(String[] args) {
 
-        ApplicationContext app = SpringApplication.run(StarrySkyKrkrApp.class,args);
+        ApplicationContext app = SpringApplication.run(SkyKrkrApp.class,args);
 
     }
 }
---
+++ krkr-service/src/main/java/com/sky/krkr/service/DemoService.java
@@ -0,0 +1,17 @@
+package com.sky.krkr.service;
+
+import com.sky.krkr.dao.DemoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    @Autowired
+    private DemoDao demoDao;
+
+    public Long query(Long id){
+        return demoDao.query(id);
+    }
+
+}
